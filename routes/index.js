23b1beb020c57adbb659debea5347f6bc24455ef
tlokotsi0 <amantusf@gmail.com>
2026-05-27 16:48:04 +0200

    Dos it work

--- routes/index.js
+++ routes/index.js
@@ -4,30 +4,17 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('../swagger.json');  
 const { userValidationRules, validate } = require('../middleware/validate');
 
-/*// Swagger UI Route
+// Swagger UI Route
 routes.use('/api-docs', swaggerUi.serve);
 routes.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
-//users routes here
+// Root Routes
 routes.get('/', usersController.getAll);
-
-routes.get('/:id', usersController.getOne);
-
-routes.post('/', usersController.createUser);
-
-routes.put('/:id', usersController.updateUser);
-
-routes.delete('/:id', usersController.deleteUser);
-
-module.exports = routes;*/
-
 routes.post('/', userValidationRules, validate, usersController.createUser);
 
-routes.put('/:id', userValidationRules, validate, usersController.updateUser);
-
-// GET and DELETE remain the same
-routes.get('/', usersController.getAll);
+// Parameterized Routes
 routes.get('/:id', usersController.getOne);
+routes.put('/:id', userValidationRules, validate, usersController.updateUser);
 routes.delete('/:id', usersController.deleteUser);
 
 module.exports = routes;
