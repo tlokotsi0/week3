334fe00f1ced2ac10d5c51aa43590ec84b8060fd
tlokotsi0 <amantusf@gmail.com>
2026-05-29 21:59:41 +0200

    Controller fixes

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,11 @@
 const routes = require('express').Router();
 const usersController = require('../controllers/users');
-const { productValidationRules, validate } = require('../middleware/validateUsers');
+const { userValidationRules, validate } = require('../middleware/validateUsers');
 
-routes.get('/', productsController.getAll);
-routes.get('/:id', productsController.getOne);
-routes.post('/', productValidationRules, validate, productsController.createProduct);
-routes.put('/:id', productValidationRules, validate, productsController.updateProduct);
-routes.delete('/:id', productsController.deleteUser);
+routes.get('/', usersController.getAll);
+routes.get('/:id', usersController.getOne);
+routes.post('/', productValidationRules, validate, usersController.createProduct);
+routes.put('/:id', productValidationRules, validate, usersController.updateProduct);
+routes.delete('/:id', usersController.deleteUser);
 
 module.exports = routes;
