43edfa4856bd9867a81422de61ec6963736051a7
tlokotsi0 <amantusf@gmail.com>
2026-06-04 10:08:04 +0200

    auth

--- middleware/authenticate.js
+++ middleware/authenticate.js
@@ -1,10 +1,8 @@
 const isAuthenticated = (req, res, next) => {
-    if (req.session.user === undefined) {
+    if (!req.isAuthenticated()) {
         return res.status(401).json("You do not have access.");
     }
     next();
 };
 
-module.exports = {
-    isAuthenticated
-}
+module.exports = { isAuthenticated };
